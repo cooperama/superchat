67b953715f53aa4c250bc59dec6abb88d5d4a0b6
cooperama <coope133@gmail.com>
2020-12-30 12:41:54 -0800

    Adds ChatRoom and messages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useRef } from "react";
 
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
 import { useAuthState } from "react-firebase-hooks/auth";
-import { usecollectionData } from "react-firebase-hooks/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
   apiKey: "AIzaSyDsAKKu8ey3Wxxs4ToGrDgfvhkVrfhVbGg",
@@ -18,13 +18,84 @@ firebase.initializeApp({
   measurementId: "G-CBDSP8KKC2",
 });
 
+// Auth & Firebase SDKs
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
+const ChatRoom = () => {
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+  const [formValue, setFormValue] = useState("");
+  const dummy = useRef();
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setFormValue("");
+
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">💋</button>
+      </form>
+    </>
+  );
+};
+
+const ChatMessage = (props) => {
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt="photo" />
+      <p>{text}</p>
+    </div>
+  );
+};
+
+const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+};
+
+const SignOut = () => {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign out</button>
+  );
+};
+
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Chatterbate</h1>
+        <SignOut />
+      </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
